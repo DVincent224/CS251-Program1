125df4ea2e44219d3c46367231faaf18e1cdae7c
DVincent224 <djvincent224@gmail.com>
2022-09-19 21:37:53 -0500

    modified:   Deck.java

--- Deck.java
+++ Deck.java
@@ -1,5 +1,3 @@
-import java.util.Random;
-
 // CS 251-401 Program 1
 // Daniel Vincent
 
@@ -8,14 +6,15 @@ public class Deck {
     public static void initDeck(boolean[] deck){
         for (int i = 0; i < deck.length; i++){
             deck[i] = true;
-            System.out.println(i + ": " + deck[i]);
+            //System.out.println(i + ": " + deck[i]);
         }
+        //System.out.println("End of Init!!!");
     }
 
     public static boolean emptyDeck(boolean[] deck){
         for (int i = 0; i < deck.length; i++){
-            if (deck[i] = true){
-                System.out.println(deck[i]);
+            if (deck[i] == true){
+                //System.out.println(deck[i]);
                 return false;
             }
         }
@@ -26,21 +25,16 @@ public class Deck {
         int card = 0;
         do{
             card = (int)Math.floor(Math.random()*(52));
-            System.out.println(card);
-        }while(!deck[card]);
-        
+        }while(!(deck[card]));
         deck[card] = false;
-        System.out.println(card);
         return card;
     }
 
     public static void printCard(int card){
-        System.out.println(card);
         String[] deckMap = new String[]{"AS","2S","3S","4S","5S","6S","7S","8S","9S","10S","JS","QS","KS",
         "AC","2C","3C","4C","5C","6C","7C","8C","9C","10C","JC","QC","KC",
         "AH","2H","3H","4H","5H","6H","7H","8H","9H","10H","JH","QH","KH",
         "AD","2D","3D","4D","5D","6D","7D","8D","9D","10D","JD","QD","KD"};
-        
         System.out.print(deckMap[card]);
     }
 
@@ -54,7 +48,6 @@ public class Deck {
         System.out.println("\nHere is a shuffled deck ...\n");
         while (!emptyDeck(myDeck)){ 
             myCard = dealCard(myDeck);
-            System.out.println(myCard);
             ++cardsThisRow;
             if (cardsThisRow <= cardsPerRow){
                 printCard(myCard);
